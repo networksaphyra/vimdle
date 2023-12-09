f58134021ec231f8f5171cc62e45c12176c3d84f
Network Saphyra <networksaphyra004@gmail.com>
2023-12-09 01:03:20 -0500

    Switched from .pdf format to .epub format because information about headings will be lost in the former

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, jsonify
 from modules.config import *
 import modules.utils as utils
+import os
 import openai
 
 app = Flask(__name__)
@@ -13,10 +14,16 @@ def home():
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["file"]
-    text_data = utils.convert_pdf_to_text(file)
+    if not utils.is_epub_file(file.filename):
+        return jsonify({"error": "Invalid File Format"}), 400
+        
+    temp_path = utils.save_to_temporary_location(file)
+    text_data = utils.extract_headings_from_epub(temp_path)
     print(text_data)
+
+    os.remove(temp_path)
     return jsonify(text_data), 200
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8888)
+    app.run(debug=True, port=8080)
--- modules/utils.py
+++ modules/utils.py
@@ -1,12 +1,31 @@
-import fitz
+from ebooklib import epub
+from bs4 import BeautifulSoup
+import tempfile
+import os
 
-def convert_pdf_to_text(pdf_file):
-    text_data = {}
+def is_epub_file(filename):
+    return filename.lower().endswith(".epub")
 
-    with fitz.open(stream=pdf_file.read(), filetype="pdf") as pdf_document:
-        for page_number in range(pdf_document.page_count):
-            page = pdf_document[page_number]
-            text = page.get_text()
-            text_data[f"Page {page_number + 1}"] = text
+def save_to_temporary_location(file):
+    #FIXME: Returning data is empty, assign it each heading
+    temp_path = os.path.join(tempfile.gettempdir(), file.filename)
+    file.save(temp_path)
+    return temp_path
 
-    return text_data
+def extract_headings_from_epub(epub_file):
+    headings_data = {}
+
+    book = epub.read_epub(epub_file)
+
+    for i, item in enumerate(book.get_items_of_type(9)):  
+        html_content = item.get_body_content().decode('utf-8', errors='ignore')
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        headings = []
+        for heading_tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+            heading_text = heading_tag.get_text(strip=True)
+            headings.append(heading_text)
+
+        headings_data[f"Page {i + 1}"] = headings
+
+    return headings_data
