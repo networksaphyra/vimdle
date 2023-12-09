61763bfa0eed7454b4ce686af516acbd55f69ae3
Network Saphyra <networksaphyra004@gmail.com>
2023-12-09 00:27:55 -0500

    Imported 'request' to fix bug of NotFound 'request'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for, jsonify
+from flask import Flask, render_template, url_for, request, jsonify
 from modules.config import *
-import fitz
+import modules.utils as utils
 import openai
 
 app = Flask(__name__)
@@ -13,19 +13,10 @@ def home():
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["file"]
-    text_data = convert_pdf_to_text(file)
+    text_data = utils.convert_pdf_to_text(file)
+    print(text_data)
     return jsonify(text_data), 200
 
-def convert_pdf_to_text(pdf_file):
-    text_data = {}
-
-    with fitz.open(stream=pdf_file.read(), filetype="pdf") as pdf_document:
-        for page_number in range(pdf_document.page_count):
-            page = pdf_document[page_number]
-            text = page.get_text()
-            text_data[f"Page {page_number + 1}"] = text
-
-    return text_data
 
 if __name__ == "__main__":
     app.run(debug=True, port=8888)
